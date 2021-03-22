056423035fc8d66c6f400c188c7bd91d7ff756ef
Mvb1122 <68322167+Mvb1122@users.noreply.github.com>
2021-03-22 10:02:51 -0600

    typo

--- #2/Main.java
+++ #2/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.print(variableThree); // print lets you put text at the end of the line.
         System.out.println(variableName + variableTwo); // println just appends "\n" to the end of the line.
         // Or well, it should. But IntelliJ doesn't want me to do it so whatever.
-        // You can also concatenate strings like in ES5. You're not able to do it using `${}` though.
+        // You can also concatenate strings like in ES6. You're not able to do it using `${}` though.
         String part1 = "Hello "; // Needs a space.
         String part2 = "Gamers."; // Javac also throws errors if you don't use semicolons at the end.
         System.out.println(part1 + part2);
