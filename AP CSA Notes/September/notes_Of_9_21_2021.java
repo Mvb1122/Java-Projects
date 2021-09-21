2ada60fa323694b6d8ab174ea67883b6db3b334f
mb1122 <68322167+Mvb1122@users.noreply.github.com>
2021-09-21 17:30:00 +0000

    vocab

---
+++ AP CSA Notes/September/notes_Of_9_21_2021.java
@@ -0,0 +1,14 @@
+// Vocab:
+  // public
+    // Variables, methods, or classes which are availible outside the class.
+  // private
+    // Variables, methods, or classes which are only availible inside the class.
+  // instance variable
+    // Variables which are assigned on an instance-wise basis; they are different
+    // for each instance (or object) of a class.
+  // attribute
+    // A characteristic of an object.
+  // has-a relationship
+    // The relationship between an object and its instance variables.
+  // data encapsulation
+    // An object-oriented programming conceppt where the details of a class are hidden from the user, it's also refered to as information hiding.
--- AP CSA Notes/compileCSA.sh
+++ AP CSA Notes/compileCSA.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "AP CSA Notes"
-javac "APCSANotes.java" "classes/SubClassExample.java" "classes/SuperClassExample.java" "August/notes_Of_8_30_2021.java" "September/notes_Of_9_2_2021.java"
+javac "APCSANotes.java" "classes/SubClassExample.java" "classes/SuperClassExample.java" "August/notes_Of_8_30_2021.java" "September/notes_Of_9_2_2021.java" "September/notes_Of_9_21_2021.java"
 java APCSANotes
